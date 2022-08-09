46386a0a4a1264e68828e39a32127b318f1a8044
Higor Machado <machado.higor98@gmail.com>
2022-08-09 18:30:23 -0300

    funcoes dos botoes JavaScript

---
+++ main.js
@@ -0,0 +1,12 @@
+
+ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 1500
+}).reveal(
+  `#home,
+  #home main,
+  #produtos #first-line,
+  #produtos #second-line,
+  #recommendation`
+)
